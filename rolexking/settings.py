6c338964b9019a44b23de0c6a5deb404b8e852c0
Prakash Rawat <87818153+prakashrawat999@users.noreply.github.com>
2025-08-22 20:59:07 +0530

    Update settings.py

--- rolexking/settings.py
+++ rolexking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bxh(h63jcf8h3zlbm=9ls--u02m8(t0a!&p_fx7fa-gvpa5fen
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,5 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
